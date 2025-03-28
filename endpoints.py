0e03e7a04933be692ecf83760e311163dcc3be5d
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2025-03-28 12:45:56 +0000

    remove js dinamico

--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,4 @@
 urls = {
-    "remotar": {"url_q": "https://remotar.com.br/search/jobs?q=", "active":0},
     "weworkremotely": {"url_q": "https://weworkremotely.com/remote-jobs/search?term=", "active":1},
-    "skipthedrive": {"url_q": "https://www.skipthedrive.com/?s=", "active":1},
-    "workingnomads": {"url_q": "https://www.workingnomads.com/jobs?tag=", "active":1},
-    "vagas": {"url_q": "https://www.vagas.com.br/vagas-", "active":0}
+    "skipthedrive": {"url_q": "https://www.skipthedrive.com/?s=", "active":1}
 }
